637f2f5e665619bf1763b93259ae98d9e4b04fd3
Timofey Potapov <timofey.potapov@otrs.com>
2021-12-10 11:25:31 +0100

    Tidy.

--- uni_convert.pl
+++ uni_convert.pl
@@ -4,11 +4,11 @@ use v5.32;
 use strict;
 use warnings;
 use utf8;
-use charnames        qw/ :full /;
-use List::Util       qw/ max /;
+use charnames qw/ :full /;
+use List::Util qw/ max /;
 use Getopt::Long;
 use Term::ANSIColor;
-use subs             qw/ r /;
+use subs qw/ r /;
 
 
 #-----------------------------------------------------------------------
@@ -18,27 +18,27 @@ use subs             qw/ r /;
 # Input.
 sub define_spec {
    {
-      "name"         => {
-                           desc      => "Input is a name.",
-                           exclusive => 1,
-                        },
-      "string"       => {
-                           desc      => "Input is a string.",
-                           exclusive => 1,
-                        },
-      "num"          => {
-                           desc      => "Input is a number.",
-                           exclusive => 1,
-                        },
-      "help"         => {
-                           desc      => "Show this help section.",
-                        },
-      "debug"        => {
-                           desc      => "Show debugging information.",
-                        },
+      "name" => {
+         desc      => "Input is a name.",
+         exclusive => 1,
+      },
+      "string" => {
+         desc      => "Input is a string.",
+         exclusive => 1,
+      },
+      "num" => {
+         desc      => "Input is a number.",
+         exclusive => 1,
+      },
+      "help" => {
+         desc => "Show this help section.",
+      },
+      "debug" => {
+         desc => "Show debugging information.",
+      },
       "list_options" => {
-                           desc      => "List available options.",
-                        },
+         desc => "List available options.",
+      },
    }
 }
 
@@ -47,22 +47,19 @@ sub build_spec_names {
 }
 
 sub build_exclusive_options {
-my $spec = define_spec();
+   my $spec = define_spec();
    grep { $spec->{$_}{exclusive} } keys %$spec;
 }
 
 sub list_options {
-   say for
-   sort
-   map {
-      s/ (?=^\w{2,}) /--/x;   # Long options.
-      s/ (?=^\w$)     /-/x;   # Short options.
+   say for sort
+     map {
+      s/ (?=^\w{2,}) /--/x;    # Long options.
+      s/ (?=^\w$)     /-/x;    # Short options.
       $_;
-   }
-   map {
-      split /\|/, $_
-   }
-   keys define_spec()->%*;
+     }
+     map { split /\|/, $_ }
+     keys define_spec()->%*;
 
    exit 1;
 }
@@ -72,13 +69,12 @@ sub build_help_options {
    my $indent = " " x 6;
 
    join "\n$indent", map {
-      my $opt = join ", ",
-      map {
-         s/ (?=^\w{2,}) /--/x;   # Long options.
-         s/ (?=^\w$)     /-/x;   # Short options.
+      my $opt = join ", ", map {
+         s/ (?=^\w{2,}) /--/x;    # Long options.
+         s/ (?=^\w$)     /-/x;    # Short options.
          $_;
-      }
-      split /\|/, $_;
+        }
+        split /\|/, $_;
 
       sprintf "%-20s %s", $opt, $spec->{$_}{desc};
    } sort keys %$spec;
@@ -104,12 +100,12 @@ sub show_help {
 sub get_options {
    my $opts = {};
 
-   GetOptions($opts, build_spec_names()) or die $!;
+   GetOptions( $opts, build_spec_names() ) or die $!;
 
-   if ($opts->{debug} ){
-      r  {
-            opts => $opts,
-            ARGV => \@ARGV,
+   if ( $opts->{debug} ) {
+      r {
+         opts => $opts,
+         ARGV => \@ARGV,
       };
    }
 
@@ -117,10 +113,11 @@ sub get_options {
 
    my @exclusive_opts = build_exclusive_options();
 
-   show_help() if not keys %$opts
-      or $opts->{help}
-      or 1 != grep {defined} @$opts{ @exclusive_opts }
-      or not @ARGV;
+   show_help()
+     if not keys %$opts
+     or $opts->{help}
+     or 1 != grep { defined } @$opts{@exclusive_opts}
+     or not @ARGV;
 
    $opts;
 }
@@ -128,7 +125,7 @@ sub get_options {
 sub get_input {
    my $have_empty_input = grep { $_ eq "" } @ARGV;
 
-   bad_input("") if $have_empty_input;
+   bad_input( "" ) if $have_empty_input;
 
    @ARGV;
 }
@@ -136,65 +133,77 @@ sub get_input {
 
 # Error reporting.
 sub bad_input {
-   _bad_x("input", @_);
+   _bad_x( "input", @_ );
 }
+
 sub bad_name {
-   _bad_x("name", @_);
+   _bad_x( "name", @_ );
 }
+
 sub _bad_x {
-   my ($what,$arg) = @_;
-   die colored("Invalid $what: ", "YELLOW") . colored("'$arg'", "RED") . "\n\n";
+   my ( $what, $arg ) = @_;
+   die colored( "Invalid $what: ", "YELLOW" )
+     . colored( "'$arg'",          "RED" ) . "\n\n";
 }
 
 
 # Unicode conversions.
 sub get_name {
-   my ($opts,$in) = @_;
+   my ( $opts, $in ) = @_;
 
    my $name = do {
-      if(   $opts->{name})   {          uc $in  }
-      elsif($opts->{string}) { string2name($in) }
-      elsif($opts->{num})    {   code2name($in) }
-      else{ show_help() }
+      if    ( $opts->{name} )   { uc $in }
+      elsif ( $opts->{string} ) { string2name( $in ) }
+      elsif ( $opts->{num} )    { code2name( $in ) }
+      else                      { show_help() }
    };
 
    if ( not defined $name ) {
-      die colored("Name cannot be determined from: ", "YELLOW") . colored($in, "RED") . "\n\n";
+      die colored( "Name cannot be determined from: ", "YELLOW" )
+        . colored( $in,                                "RED" ) . "\n\n";
    }
 
    $name;
 }
 
 sub name_2_line {
-   my ($name) = @_;
-   my $string  = name2string($name) // bad_name($name);
-   my $uni     = name2uni($name);
-   my $hex     = name2hex($name);
-   my $dec     = name2dec($name);
+   my ( $name ) = @_;
+   my $string   = name2string( $name ) // bad_name( $name );
+   my $uni      = name2uni( $name );
+   my $hex      = name2hex( $name );
+   my $dec      = name2dec( $name );
 
    # Make white space and escapes easier to see.
    $string =~ s/[\s\p{xPosixCntrl}]/ /g;
 
-   [
-      $string,
-      $name,
-      $uni,
-      $hex,
-      $dec,
-   ];
+   [ $string, $name, $uni, $hex, $dec, ];
 }
 
-sub string2name{ charnames::viacode ord shift }          # ❄                      -> SNOWFLAKE
-sub code2name  { charnames::viacode( shift )  }          # U+2744, 0x2744, 10052  -> SNOWFLAKE
-sub name2string{ charnames::string_vianame( shift ) }    # SNOWFLAKE              -> ❄
-sub name2uni   { sprintf "U+%x", name2dec( shift )  }    # SNOWFLAKE              -> U+2744
-sub name2hex   { sprintf "%#x",  name2dec( shift )  }    # SNOWFLAKE              -> 0x2744
-sub name2dec   { charnames::vianame( shift ) }           # SNOWFLAKE              -> 10052
+sub string2name {
+   charnames::viacode ord shift;
+}    # ❄                      -> SNOWFLAKE
+
+sub code2name {
+   charnames::viacode( shift );
+}    # U+2744, 0x2744, 10052  -> SNOWFLAKE
+
+sub name2string {
+   charnames::string_vianame( shift );
+}    # SNOWFLAKE              -> ❄
+
+sub name2uni {
+   sprintf "U+%x", name2dec( shift );
+}    # SNOWFLAKE              -> U+2744
+
+sub name2hex {
+   sprintf "%#x", name2dec( shift );
+}    # SNOWFLAKE              -> 0x2744
+sub name2dec { charnames::vianame( shift ) }   # SNOWFLAKE              -> 10052
 
 
 # Render.
 sub render_lines {
-   my $max    = get_max_length(@_);
+   my $max    = get_max_length( @_ );
    my $format = sprintf define_raw_format(), @$max;
 
    for ( @_ ) {
@@ -203,9 +212,9 @@ sub render_lines {
 }
 
 sub define_raw_format {
-   colored("%%%ss",               "RED")             . " " .
-   colored("%%-%ss",              "YELLOW")          . " " .
-   colored("%%-%ss %%-%ss %%%ss", "ON_BRIGHT_BLACK") . "\n";
+   colored( "%%%ss", "RED" ) . " "
+     . colored( "%%-%ss",              "YELLOW" ) . " "
+     . colored( "%%-%ss %%-%ss %%%ss", "ON_BRIGHT_BLACK" ) . "\n";
 }
 
 sub get_max_length {
@@ -214,9 +223,7 @@ sub get_max_length {
 
    my @max = map {
       my $col = $_;
-      max map {
-         length $_[$_][$col];
-      } 0 .. $last_row;
+      max map { length $_[$_][$col]; } 0 .. $last_row;
    } 0 .. $last_column;
 
    \@max;
@@ -226,11 +233,7 @@ sub get_max_length {
 # Debug.
 sub r {
    require Data::Dumper;
-   say Data::Dumper->new([@_])
-         ->Terse(1)
-         ->Indent(1)
-         ->Sortkeys(1)
-         ->Dump;
+   say Data::Dumper->new( [@_] )->Terse( 1 )->Indent( 1 )->Sortkeys( 1 )->Dump;
 }
 
 
@@ -239,16 +242,16 @@ sub r {
 #-----------------------------------------------------------------------
 
 say "";
-my $opts    = get_options();
-my ($input) = get_input();
-my @process = $opts->{string} ? (split //, $input) : ($input);
+my $opts      = get_options();
+my ( $input ) = get_input();
+my @process   = $opts->{string} ? ( split //, $input ) : ( $input );
 my @lines;
 
 for ( @process ) {
-   my $name = get_name($opts,$_);   # Normalize to a name.
-   push @lines, name_2_line($name); # Build other parts.
+   my $name = get_name( $opts, $_ );     # Normalize to a name.
+   push @lines, name_2_line( $name );    # Build other parts.
 }
-render_lines @lines if @lines;      # Format and render.
+render_lines @lines if @lines;           # Format and render.
 say "";
 
 #-----------------------------------------------------------------------
